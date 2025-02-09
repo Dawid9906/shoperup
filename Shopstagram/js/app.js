aa37fd807629b1e0946072c89f0b08f263d70e07
Dawid9906 <dawid.kargul1@gmail.com>
2025-02-09 21:46:02 +0100

    added styles as sass changed js functions names

--- Shopstagram/js/app.js
+++ Shopstagram/js/app.js
@@ -1,11 +1,11 @@
 let button = document.getElementById("button");
 
 button.addEventListener("click", function () {
-  let z1 = document.querySelector(".z-1");
-  let z2 = document.querySelector(".z-2");
+  let modalOverlay = document.querySelector(".z-1");
+  let modal = document.querySelector(".z-2");
 
-  z1.classList.add("d-none");
-  z2.classList.add("d-none");
+  modalOverlay.classList.add("d-none");
+  modal.classList.add("d-none");
 });
 
 let buttonLoadMore = document.getElementById("load-more-button");
